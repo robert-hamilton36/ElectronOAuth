5c18ec83addf1b17d29e208613bdeccce43ab955
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-07-12 12:59:08 +1200

    setup to bundle main

---
+++ src/main/webpack.main.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const webpack = require('webpack')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  entry: {
+    main: './src/main/main.ts',
+  },
+  output: {
+    path: path.join(__dirname, '..', '..'),
+    filename: 'main.js'
+  },
+  mode: 'development',
+  plugins: [
+    new Dotenv({
+    path: './.env'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.CLIENT_ID': JSON.stringify(process.env.CLIENT_ID),
+      'process.env.CLIENT_SECRET': JSON.stringify(process.env.CLIENT_SECRET),
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.ts']
+  },
+  target: 'electron-main'
+}
